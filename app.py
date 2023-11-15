a7f257f824460dd742cdd286234ea6b1cbaea295
macbookalex <golovatyalex@mail.ru>
2023-11-15 11:39:20 +0000

    First pages

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/', methods=['GET', 'POST'])
+def starter():
+    if request.method == 'POST':
+        if request.form.get('action'):
+            return redirect('/deposit')
+        elif request.form.get('action') == 'withdraw':
+            return redirect('/withdraw')
+        elif request.form.get('action') == 'balance':
+            return redirect('/balance')
+    return render_template('main.html')
 
+
+@app.route('/deposit', methods=['GET', 'POST'])
+def deposit():
+    return render_template('deposit.html')
 
 if __name__ == '__main__':
     app.run()
